d2adcc1069d5d79b338293ade6cca166ec985029
Alexey V. Ryazhskikh <alexey.r@toptal.com>
2020-08-24 22:23:55 +0300

    Empty projects structure

---
+++ LogBook.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogBook.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
